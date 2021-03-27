eb473f05be0e42538ecb49adc76bd2ae40dec889
Lu <luca-pravez@web.de>
2021-03-27 10:12:24 +0100

    Storing the last move in the meta data board

--- engine/src/main/java/lichessbot/engine/impl/game/MetaDataBitboard.java
+++ engine/src/main/java/lichessbot/engine/impl/game/MetaDataBitboard.java
@@ -5,13 +5,75 @@ public class MetaDataBitboard {
   public static void setWhiteTurn(boolean[] metaDataBitboard) {
     metaDataBitboard[0] = true;
   }
+
   public static void setBlackTurn(boolean[] metaDataBitboard) {
     metaDataBitboard[0] = false;
   }
-  
 
   public static boolean isWhiteTurn(boolean[] metaDataBitboard) {
     return metaDataBitboard[0];
+  }
+
+  public static String getLastMove(boolean[] metaDataBitboard) {
+    if (metaDataBitboard[1]) {
+      return readCastelingMove(metaDataBitboard);
+    }
+    return readMove(metaDataBitboard);
+  }
+
+  private static String readMove(boolean[] metaDataBitboard) {
+    StringBuilder move = new StringBuilder(4);
+    move.append(readChar(metaDataBitboard, 2, 'a')).append(readChar(metaDataBitboard, 5, '0'));
+    move.append(readChar(metaDataBitboard, 8, 'a')).append(readChar(metaDataBitboard, 11, '0'));
+    return move.toString();
+  }
+
+  private static char readChar(boolean[] metaDataBitboard, int offset, char toCompare) {
+    char symbol = toCompare;
+    if (metaDataBitboard[offset]) {
+      symbol += 0x4;
+    }
+    if (metaDataBitboard[offset + 1]) {
+      symbol += 0x2;
+    }
+    if (metaDataBitboard[offset + 2]) {
+      symbol += 0x1;
+    }
+    return symbol;
+  }
+
+  private static String readCastelingMove(boolean[] metaDataBitboard) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public static void setLastMove(boolean[] metaDataBitboard, String move) {
+    char[] moveChars = move.toCharArray();
+    if (moveChars[0] == '0') {
+      setCasteling(metaDataBitboard, moveChars);
+    } else {
+      setMove(metaDataBitboard, moveChars);
+    }
+  }
+
+  private static void setMove(boolean[] metaDataBitboard, char[] moveChars) {
+    metaDataBitboard[1] = false;
+    setForSymbol(metaDataBitboard, moveChars[0], 'a', 2);
+    setForSymbol(metaDataBitboard, moveChars[1], '0', 5);
+    setForSymbol(metaDataBitboard, moveChars[2], 'a', 8);
+    setForSymbol(metaDataBitboard, moveChars[3], '0', 11);
+  }
+
+  private static void setForSymbol(boolean[] metaDataBitboard, char moveChar, char toCompareChar, int offset) {
+    int position = moveChar - toCompareChar;
+    metaDataBitboard[offset] = (position & 0x4) == 0x4;
+    metaDataBitboard[offset + 1] = (position & 0x2) == 0x2;
+    metaDataBitboard[offset + 2] = (position & 0x1) == 0x1;
+  }
+
+  private static void setCasteling(boolean[] metaDataBitboard, char[] moveChars) {
+    metaDataBitboard[1] = true;
+    metaDataBitboard[2] = moveChars.length > 3;
   }
 
 }
---
+++ engine/src/test/java/lichessbot/engine/MetaDataBitboardTest.java
@@ -0,0 +1,58 @@
+package lichessbot.engine;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import lichessbot.engine.impl.common.BitBoardFactory;
+import lichessbot.engine.impl.game.MetaDataBitboard;
+
+public class MetaDataBitboardTest {
+
+  private boolean[] _metaDataBitboard;
+
+  @BeforeEach
+  void setup() {
+    _metaDataBitboard = BitBoardFactory.GeneralUsage.createEmptyBitboard();
+  }
+
+  @Test
+  void testSetBlackTurn() {
+    MetaDataBitboard.setBlackTurn(_metaDataBitboard);
+    assertThat(MetaDataBitboard.isWhiteTurn(_metaDataBitboard)).isEqualTo(false);
+  }
+
+  @Test
+  void testSetWhiteTurn() {
+    MetaDataBitboard.setWhiteTurn(_metaDataBitboard);
+    assertThat(MetaDataBitboard.isWhiteTurn(_metaDataBitboard)).isEqualTo(true);
+  }
+
+  @Test
+  void testSetLastMove() {
+    MetaDataBitboard.setLastMove(_metaDataBitboard, "e4e5");
+    List<Integer> expectedSetIndices = Arrays.asList(2, 5, 8, 11, 13);
+    List<Integer> setPosition = new ArrayList<>();
+    for (int setConfigurationIndex = 0; setConfigurationIndex < _metaDataBitboard.length; setConfigurationIndex++) {
+      if (_metaDataBitboard[setConfigurationIndex]) {
+        setPosition.add(setConfigurationIndex);
+      }
+    }
+    assertThat(setPosition).containsExactlyInAnyOrderElementsOf(expectedSetIndices);
+  }
+
+  @Test
+  void testGetLastMove() {
+    List<Integer> expectedSetIndices = Arrays.asList(2, 5, 8, 11, 13);
+    for (Integer configurationIndex : expectedSetIndices) {
+      _metaDataBitboard[configurationIndex] = true;
+    }
+    String lastMove = MetaDataBitboard.getLastMove(_metaDataBitboard);
+    assertThat(lastMove).isEqualTo("e4e5");
+  }
+}
