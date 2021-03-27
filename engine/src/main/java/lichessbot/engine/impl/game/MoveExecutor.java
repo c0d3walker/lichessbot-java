e382d940df87535a60e31d1a03b761c4f9183769
Lu <luca-pravez@web.de>
2021-03-27 14:02:58 +0100

    Integrating the special moves into the game model

--- engine/src/main/java/lichessbot/engine/impl/game/MoveExecutor.java
+++ engine/src/main/java/lichessbot/engine/impl/game/MoveExecutor.java
@@ -4,24 +4,25 @@ import java.util.Objects;
 
 import lichessbot.engine.IStatus;
 import lichessbot.engine.impl.common.FieldConverter;
+import lichessbot.engine.impl.common.SpecialMoveUtility;
 import lichessbot.engine.impl.common.Status;
 
 public class MoveExecutor {
-  
-  public static IStatus execute(Position position,String move) {
+
+  public static IStatus execute(Position position, String move) {
     int fromField = FieldConverter.toIndex(move.substring(0, 2));
     int toField = FieldConverter.toIndex(move.substring(2, 4));
-    IStatus updateFigureDataStatus = updateFigureData(position,fromField, toField);
+    IStatus updateFigureDataStatus = updateFigureData(position, fromField, toField);
     if (updateFigureDataStatus.isOK()) {
-      updatePlayerData(position,fromField, toField);
+      updatePlayerData(position, fromField, toField);
       MetaDataBitboard.setLastMove(position.getMetaDataBitboard(), move);
     }
     return updateFigureDataStatus;
   }
-  
 
-  private static IStatus updateFigureData(Position position,int fromField, int toField) {
-    boolean isUpdatePending = !updateBitboard(position, position.getPawnBitboard(), fromField, toField)//
+  private static IStatus updateFigureData(Position position, int fromField, int toField) {
+    boolean isUpdatePending = !isSpecialMove(position, fromField, toField) && //
+        !updateBitboard(position, position.getPawnBitboard(), fromField, toField)//
         && !updateBitboard(position, position.getCastelBitboard(), fromField, toField)//
         && !updateBitboard(position, position.getKnightBitboard(), fromField, toField) //
         && !updateBitboard(position, position.getBishopBitboard(), fromField, toField) //
@@ -33,6 +34,30 @@ public class MoveExecutor {
     return new Status(true, "Move accepted", "");
   }
 
+  private static boolean isSpecialMove(Position position, int fromField, int toField) {
+    if (SpecialMoveUtility.isAuPassant(position, fromField, toField)) {
+      executeAuPassant(position, fromField, toField);
+      return true;
+    }
+    return false;
+  }
+
+  private static void executeAuPassant(Position position, int fromField, int toField) {
+    boolean[] pawnBitboard = position.getPawnBitboard();
+    boolean[] whiteBitboard = position.getWhiteBitboard();
+
+    pawnBitboard[fromField] = false;
+    pawnBitboard[toField] = true;
+
+    String lastMove = MetaDataBitboard.getLastMove(position.getMetaDataBitboard());
+    String lastTarget = lastMove.substring(2);
+    int lastTargetIndex = FieldConverter.toIndex(lastTarget);
+    pawnBitboard[lastTargetIndex] = false;
+    whiteBitboard[lastTargetIndex] = false;
+
+  }
+
+// TODO can be removed?
   private static void emptyField(Position position, int toField) {
     position.getPawnBitboard()[toField] = false;
     position.getCastelBitboard()[toField] = false;
@@ -52,7 +77,7 @@ public class MoveExecutor {
     return false;
   }
 
-  private static void updatePlayerData(Position position,int fromField, int toField) {
+  private static void updatePlayerData(Position position, int fromField, int toField) {
     boolean isWhiteTurn = MetaDataBitboard.isWhiteTurn(position.getMetaDataBitboard());
     boolean[] whiteBitboard = position.getWhiteBitboard();
     if (isWhiteTurn) {
